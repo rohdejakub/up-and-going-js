c7001f0226542195d8181b0f3a26d6d1171eeca8
Jakub Rohde <rohdejakub@gmail.com>
2018-09-25 23:57:47 +0200

    chapter nr 2

---
+++ ch2/Objects_arrays.js
@@ -0,0 +1,29 @@
+
+var obj = {
+	a: "hello world",
+	b: 42,
+	c: true
+};
+
+obj.a;		// "hello world"
+obj.b;		// 42
+obj.c;		// true
+
+obj["a"];	// "hello world"
+obj["b"];	// 42
+obj["c"];	// true
+
+
+var arr = [
+	"hello world",
+	42,
+	true
+];
+
+arr[0];			// "hello world"
+arr[1];			// 42
+arr[2];			// true
+arr.length;		// 3
+
+typeof arr;		// "object"
+
